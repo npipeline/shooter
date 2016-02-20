daa949b1c05b1568f82c7f4804ad32796c2df6d6
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2016-02-20 21:34:21 +1030

    Add concrete ship controllers, placeholder sprites and test scene

---
+++ Assets/Scripts/ExampleBotShipController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleBotShipController : ShipController
+{
+    public float fireDelay;
+
+    private Vector3 startPosition;
+    private float angle = 0;
+    private float lastFireTime;
+
+    protected override void Initialize()
+    {
+        startPosition = transform.position;
+        lastFireTime = Time.time;
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+        Firing();
+    }
+
+    // Example spinning ship
+    private void Movement()
+    {
+        const float radius = 2;
+
+        float horizontal = Mathf.Sin(angle) * radius;
+        float vertical = Mathf.Cos(angle) * radius;
+        angle += Time.fixedDeltaTime;
+
+        movementController.Move(horizontal, vertical);
+    }
+
+    // Example firing
+    private void Firing()
+    {
+        var lastFireTimeDelta = Time.time - lastFireTime;
+        if (lastFireTimeDelta > fireDelay)
+        {
+            Fire();
+            lastFireTime = Time.time;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerShipController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerShipController : ShipController
+{
+    protected override void Initialize()
+    {
+        // Nothing to do
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+        bool fire = Input.GetButton("Fire1");
+
+        movementController.Move(horizontalAxis, verticalAxis);
+
+        if (fire)
+        {
+            Fire();
+        }
+    }
+}
