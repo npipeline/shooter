e5a813b40ff80951e3ea869aeda9af959f950dd3
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2016-02-21 17:30:59 +1030

    Lock player ship within screen bounds

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,6 +5,9 @@ public class MovementController : MonoBehaviour
 {
     public float horizontalSpeed;
     public float verticalSpeed;
+    public bool keepWithinBounds;
+    public float screenWidth;
+    public float screenHeight;
 
     private new Rigidbody rigidbody;
 
@@ -15,13 +18,21 @@ public class MovementController : MonoBehaviour
 
     public void Move(float horizontalAxis, float verticalAxis)
     {
-        // TODO(jaween): Keep the player's ship within the bounds
-
         var movement =
         Vector3.right * horizontalAxis * horizontalSpeed +
         Vector3.up * verticalAxis * verticalSpeed;
 
-        rigidbody.MovePosition(transform.position + 
-            movement * Time.fixedDeltaTime);
+        Vector3 newPosition = transform.position + 
+            movement * Time.fixedDeltaTime;
+        
+        if (keepWithinBounds)
+        {
+            float xBound = screenWidth / 2.0f;
+            float yBound = screenHeight / 2.0f;
+            newPosition.x = Mathf.Clamp(newPosition.x, -xBound, xBound);
+            newPosition.y = Mathf.Clamp(newPosition.y, -yBound, yBound);
+        }
+
+        rigidbody.MovePosition(newPosition);
     }
 }
