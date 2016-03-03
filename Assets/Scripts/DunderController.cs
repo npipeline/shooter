2e860e73498dcf84f2e50e5e5b732d9d73a5b34a
Ethan Elliott <spindash712@Hotmail.com>
2016-03-03 11:41:01 -0600

    Adding Dunder, modifying Movement Controller
    Dunder added, added events to movement controller and a move type

---
+++ Assets/Scripts/DunderController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DunderController : ShipController
+{
+	public float moveDelay;
+	private Rigidbody2D PlayerBody;
+	private new Rigidbody2D rigidbody;
+	bool CanMove = true;
+
+	protected override void Initialize ()
+	{
+		rigidbody = GetComponent<Rigidbody2D> ();
+		PlayerShipController Player = FindObjectOfType<PlayerShipController> ();
+		PlayerBody = Player.gameObject.GetComponent<Rigidbody2D> ();
+		movementController.Moved += ctlr_Moved;
+	}
+
+	void ctlr_Moved (object sender, System.EventArgs e)
+	{
+		Fire();
+		StartCoroutine(SetCanMove());
+	}
+
+	private void FixedUpdate ()
+	{
+		Movement ();
+	}
+
+	private void Movement ()
+	{
+		if (CanMove) 
+		{
+			Vector2 newPosition = new Vector2 (PlayerBody.transform.position.x, rigidbody.transform.position.y);		
+			movementController.MoveTo (newPosition);
+			CanMove = false;
+		}
+	}
+
+	private IEnumerator SetCanMove()
+	{
+		yield return new WaitForSeconds(moveDelay);
+		CanMove = true;
+	}
+
+}
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,55 +1,111 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class MovementController : MonoBehaviour
 {
-    public float horizontalSpeed;
-    public float verticalSpeed;
-    public bool keepWithinBounds;
-    public float screenWidth;
-    public float screenHeight;
+	#region Events
+	public delegate void MovedEventHandler(object sender, EventArgs e);
 
-    private new Rigidbody2D rigidbody;
-    private float horizontalSum = 0;
-    private float verticalSum = 0;
+	public event MovedEventHandler Moved;
+	#endregion
 
-	private void Start()
-    {
-        rigidbody = GetComponent<Rigidbody2D>();
+	public float horizontalSpeed;
+	public float verticalSpeed;
+	public bool keepWithinBounds;
+	public float screenWidth;
+	public float screenHeight;
+	private MoveType NextMoveType;
+	private new Rigidbody2D rigidbody;
+	private float horizontalSum = 0;
+	private float verticalSum = 0;
+	private Vector2 newPosition;
+
+	private void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody2D> ();
+		NextMoveType = MoveType.None;
 	}
 
-    private void FixedUpdate()
-    {
-        MoveInternal();
-    }
+	private void FixedUpdate ()
+	{
+		MoveInternal ();
+	}
 
-    private void MoveInternal()
-    {
-        var movement =
-            Vector2.right * horizontalSum * horizontalSpeed +
-            Vector2.up * verticalSum * verticalSpeed;
+	private void MoveInternal ()
+	{
+		if (NextMoveType != MoveType.None) 
+		{
+			switch (NextMoveType) 
+			{
+				case MoveType.Move:			
+					var movement =
+	           		    Vector2.right * horizontalSum * horizontalSpeed +
+						Vector2.up * verticalSum * verticalSpeed;
 
-        horizontalSum = 0;
-        verticalSum = 0;
-        
-        Vector2 newPosition = rigidbody.position + 
-            movement * Time.fixedDeltaTime;
-        
-        if (keepWithinBounds)
-        {
-            float xBound = screenWidth / 2.0f;
-            float yBound = screenHeight / 2.0f;
-            newPosition.x = Mathf.Clamp(newPosition.x, -xBound, xBound);
-            newPosition.y = Mathf.Clamp(newPosition.y, -yBound, yBound);
-        }
+					horizontalSum = 0;
+					verticalSum = 0;
+	        
+					newPosition = rigidbody.position + 
+						movement * Time.fixedDeltaTime;
 
-        rigidbody.MovePosition(newPosition);
-    }
+				
+				
+					if (keepWithinBounds) {
+						float xBound = screenWidth / 2.0f;
+						float yBound = screenHeight / 2.0f;
+						newPosition.x = Mathf.Clamp (newPosition.x, -xBound, xBound);
+						newPosition.y = Mathf.Clamp (newPosition.y, -yBound, yBound);
+					}
+					
+					rigidbody.MovePosition (newPosition);
+					break;
 
-    public void Move(float horizontalAxis, float verticalAxis)
-    {
-        // Holds onto all the inputs until FixedUpdate()
-        horizontalSum += horizontalAxis;
-        verticalSum += verticalAxis;
-    }
+				case MoveType.MoveTo:
+					if (keepWithinBounds) {
+						float xBound = screenWidth / 2.0f;
+						float yBound = screenHeight / 2.0f;
+						newPosition.x = Mathf.Clamp (newPosition.x, -xBound, xBound);
+						newPosition.y = Mathf.Clamp (newPosition.y, -yBound, yBound);
+					}
+					rigidbody.position = newPosition;
+//					Vector2.MoveTowards(rigidbody.position, newPosition, 0.0f);
+					break;
+
+				default: 
+					break;
+			}
+			ThrowMovedEvent();
+			NextMoveType = MoveType.None;
+		}
+		
+	}
+
+	private void ThrowMovedEvent()
+	{
+		if(Moved != null)
+			Moved(this, new EventArgs());
+	}
+
+	public void Move (float horizontalAxis, float verticalAxis)
+	{
+		// Holds onto all the inputs until FixedUpdate()
+		horizontalSum += horizontalAxis;
+		verticalSum += verticalAxis;
+		NextMoveType = MoveType.Move;
+
+	}
+
+	public void MoveTo (Vector2 Position)
+	{
+		newPosition = Position;
+		NextMoveType = MoveType.MoveTo;
+	}
+}
+
+public enum MoveType
+{
+	None,
+	Move,
+	MoveTo
 }
